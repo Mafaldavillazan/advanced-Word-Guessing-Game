edaa43cd9d18c396ded10cb75f0b7fe3465f3a92
Mafalda <mafalda.villazan@gmail.com>
2019-07-17 17:10:22 -0700

    Adding the basic pages

---
+++ letter.js
@@ -0,0 +1,11 @@
+//Creates constructor
+function Letter(string, ){
+    this.string = string.split("")
+    //Bolean that takes every letter and puts into boolan
+   arrLetters = []
+   arrLetters.push(string)
+
+   for(var i = 0; i<arrLetters.length;i++){
+       
+   }
+}
